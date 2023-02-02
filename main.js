17a00df1ffbf6783456e4ae6138b170bba8d27a4
anzor1qqqqq <anzor.shadzhe@gmail.com>
2023-02-03 00:40:26 +0300

    modified:   main.js

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 'use strict'
 
-let text = 'hello my friends!';
+/* const title = prompt('Как называется ваш проект?');
+const screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные'); 
+const screenPrice = prompt('Сколько будет стоить данная работа?', '12000');
+const adaptive = confirm('Нужен ли адаптив на сайте?');
 
-console.log(text.split(''));
+ */
