c3be74c0c681321558947296afca3427be085b92
anzor1qqqqq <anzor.shadzhe@gmail.com>
2023-02-02 14:26:20 +0300

    динамическая типизация

--- main.js
+++ main.js
@@ -1,17 +1,5 @@
 'use strict'
 
-const title = 'glo-project';
-const screens = 'Простые, Сложные, Интерактивные';
-const screenPrice = 8;
-const rollback = 99;
-const fullPrice = 15000;
-const adaptive = false;
+let text = 'hello my friends!';
 
-console.log(typeof(title), typeof(fullPrice), typeof(adaptive));
-console.log(screens.length);
-console.log( `Стоимость верстки экранов ${screenPrice} рублей и Стоимость разработки сайта ${fullPrice} рублей`);
-
-const arr = screens.toLowerCase().split();
-console.log(arr);
-
-console.log((fullPrice * (rollback/100)));
+console.log(text.split(''));
