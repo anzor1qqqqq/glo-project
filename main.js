23c4ea52d2b1695915b48e47ea77c8ae120fb912
anzor1qqqqq <anzor.shadzhe@gmail.com>
2023-02-03 23:02:23 +0300

    modified:   main.js

--- main.js
+++ main.js
@@ -1,30 +1,3 @@
 'use strict'
 
-const title = prompt('Как называется ваш проект?'),
-    screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные'),
-    adaptive = confirm('Нужен ли адаптив на сайте?'),
-    screenPrice = prompt('Сколько будет стоить данная работа?', '12000');
-
-let fullPrice = +screenPrice; 
-
-switch (true) {
-    case fullPrice > 30000:
-        fullPrice -= fullPrice * 0.1;
-        alert('Сумма заказа составила больше 30000 рублей, в связи с этим делаем вам скидку 10%');
-        break;
-    case fullPrice > 15000 && fullPrice < 30000:
-        fullPrice -= fullPrice * 0.05;
-        alert('Сумма заказа составила больше 15000 рублей, в связи с этим делаем вам скидку 5%');
-        break;
-    case fullPrice < 15000 && fullPrice > 0: 
-        alert('Скидка не предусмотрена');
-        break;
-    case fullPrice <= 1000:
-        alert('Что-то пошло не так');
-        break;
-    default:
-        break;
-}; 
-
-const servicePercentPrice = Math.ceil(fullPrice - (fullPrice * 0.1));
-console.log('servicePercentPrice: ', servicePercentPrice);
+console.log(23);
