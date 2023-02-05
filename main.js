03ec1d68a1bed79324dff4adedc9af25387cbdc7
anzor1qqqqq <anzor.shadzhe@gmail.com>
2023-02-05 14:10:26 +0300

    modified:   main.js

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ let title = prompt('Как называется ваш проект?'),
     adaptive = confirm('Нужен ли адаптив на сайте?'),
     screenPrice = prompt('Сколько будет стоить данная работа?', '12000');
 
+console.log('screens: ', screens);
+
 const allServicePrices = function getAllServicePrices (a, b) {
     return a + b;
 };
@@ -15,8 +17,26 @@ function getFullPrice(a, b) {
     return a + b;
 };
 
-const fullPrice = getFullPrice(+screenPrice, allServicePrices(service1, service2));
-console.log('fullPrice: ', fullPrice);
+let fullPrice = getFullPrice(+screenPrice, allServicePrices(service1, service2));
+
+switch (true) {
+    case fullPrice > 30000:
+        fullPrice -= fullPrice * 0.1;
+        alert('Сумма заказа составила больше 30000 рублей, в связи с этим делаем вам скидку 10%');
+        break;
+    case fullPrice > 15000 && fullPrice < 30000:
+        fullPrice -= fullPrice * 0.05;
+        alert('Сумма заказа составила больше 15000 рублей, в связи с этим делаем вам скидку 5%');
+        break;
+    case fullPrice < 15000 && fullPrice > 0: 
+        alert('Скидка не предусмотрена');
+        break;
+    case fullPrice <= 1000:
+        alert('Что-то пошло не так');
+        break;
+    default:
+        break;
+};
 
 function getTitle() {
     if (title)  {
@@ -24,9 +44,12 @@ function getTitle() {
         document.title = title[0].toUpperCase() + title.slice(1).toLowerCase(); 
     }
 };
-getTitle()
-
 
 const servicePercentPrice = function getServicePercentPrices() {
-    
+    return Math.ceil(fullPrice - (fullPrice * 0.1));
 };
+
+getTitle();
+servicePercentPrice();
+
+console.log('servicePercentPrice: ', servicePercentPrice());
